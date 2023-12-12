2d9775fb324d56faedb41aeeeb531eed0f2cc69f
Antonio Brito <antubrito@gmail.com>
2023-12-11 20:57:10 -0500

    Add project files.

---
+++ Generico.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_C_
+{
+    public class Generico<T> where T : class
+    {
+        private List<T>  IList = new List<T>();
+
+
+
+      public void  add(  T[] Items )
+
+      {
+
+
+
+            IList.AddRange( Items );
+
+        }
+
+
+        public T getItem(T Item)
+
+        {
+
+
+
+            return IList.FirstOrDefault(Item);
+
+        }
+
+
+        public List<T> getAllSorted()
+
+        {
+
+
+
+            return IList.Sort();
+
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exam_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+
+
+
+
+    }
+}
